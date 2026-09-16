3a08c71403eb5c0980079bf0bc9405c7ab3d60a9
jaziellramirez-beep <jaziellramirez@gmail.com>
2026-09-16 17:49:09 -0500

    lab 3
    Refactored distance calculation functions and improved output formatting.

--- main (18).cpp
+++ main (18).cpp
@@ -6,48 +6,57 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 
+//calculates the approximate distance from the Earth to the Sun
+// amount of time sunlight takes to reach Earth. 
+
 #include <iostream>
 using namespace std;
 
-//1
-double conversiontime(double minutes,double seconds){
+double convertToSeconds(int minutes, int seconds);
+double calculateDistance(double speed, double timeInSeconds);
+double convertKmToMiles(double kilometers);
+
+//function is time
+double convertToSeconds(double minutes, double seconds) {
     return (minutes * 60) + seconds;
 }
-//2
-double distance(double speed,double tseconds){
-    return speed * tseconds;
+
+// Function is distance
+double calculateDistance(double speed, double timeInSeconds) {
+    return speed * timeInSeconds;
 }
-//3
-double kmtom(double kilometer){
-    double Km_to_mile = 0.621371;
-    return kilometer*Km_to_mile;
 
-//create 2 paramet func
-double light = 299,792.0;
-//get the time
-int minutes 8;
-int seconds 19;
-//get total time
-totaltime = conversiontime(double minutes,double seconds);
-//distance
-distance(speed,tseconds);
-//mile
-kmtom(double kilometer);
-
-//what they see
-
-   cout << "--- Sun to Earth Distance Calculator ---" << endl;
+// Function to convert kilometers to miles 
+double convertKmToMiles(double kilometers) {
+    const double KM_TO_MILES_CONVERSION = 0.621371;
+    return kilometers * KM_TO_MILES_CONVERSION;
+}
+int main() {
+    // 1. Store the speed of light in a variable (km/s)
+    const double SPEED_OF_LIGHT = 299792.0;
+    
+    // Sunlight travel time: 8 minutes and 19 seconds
+    int minutes = 8;
+    int seconds = 19;
+    
+    // 2. Call function to convert the travel time to seconds
+    double totalSeconds = convertToSeconds( minutes, seconds);
+    
+    // 3. Call function to calculate the distance in kilometers
+    double distanceKm = calculateDistance(SPEED_OF_LIGHT, totalSeconds);
+    
+    // 4. Call function to convert the distance to miles
+    double distanceMiles = convertKmToMiles(distanceKm);
+    
+    // 5. Display the results clearly
+    cout << "--- Time for sunlight to hit earth ---" << endl;
     cout << "Time taken for sunlight to reach Earth: " << minutes << " minutes and " << seconds << " seconds" <<endl;
-    cout << "Total travel time in seconds: " << totalSeconds << " seconds" <<endl;
-    cout << "----------------------------------------" <<endl;
-    cout << "Approximate Distance in Kilometers: " << distanceKm << " km" <<endl;
-    cout << "Approximate Distance in Miles:      " << distanceMiles << " miles" <<endl;
-
-
-
-int main()
-{
-    std::cout<<"Hello World";
-
+    cout << "Total travel time in seconds: " << totalSeconds << " seconds" << endl;
+    cout << "----------------------------------------" << endl;
+   
+   cout<<"Distance from earth to the sun"<<endl;
+   cout << "Approximate Distance in Kilometers: " << distanceKm << " km" <<endl;
+    cout << "Approximate Distance in Miles:      " << distanceMiles << " miles" << endl;
+    
     return 0;
 }
